c367ad485e262effc44ba784c51b95cbe0b26626
soso-light <80017031+soso-light@users.noreply.github.com>
2021-07-05 13:19:15 +0900

    hahahah...

--- app/src/main/java/com/example/android/materialme/Sport.java
+++ app/src/main/java/com/example/android/materialme/Sport.java
@@ -22,8 +22,13 @@ package com.example.android.materialme;
 class Sport {
 
     // Member variables representing the title and information about the sport.
+    private final int imageResource;
     private String title;
     private String info;
+
+    public int getImageResource() {
+        return imageResource;
+    }
 
     /**
      * Constructor for the Sport data model.
@@ -31,9 +36,10 @@ class Sport {
      * @param title The name if the sport.
      * @param info Information about the sport.
      */
-    Sport(String title, String info) {
+    Sport(String title, String info, int imageResource) {
         this.title = title;
         this.info = info;
+        this.imageResource = imageResource;
     }
 
     /**
